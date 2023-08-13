53d9483c06ef1f54c356772fb7af88abc63e2e6c
rai <mj82xx@gmail.com>
2023-08-14 02:01:39 +0900

    feat: indexページに、リンク集と3Dモデルを表示

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    viewTransitions: true
+    viewTransitions: true,
+    assets: true
   },
   integrations: [tailwind({
     applyBaseStyles: false
