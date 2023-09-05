69e2459dc851e395ae885898fe6961dbe62a4ce9
rai <mj82xx@gmail.com>
2023-09-05 18:17:16 +0900

    iconifyを導入

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,7 +1,5 @@
 import { getCollection } from "astro:content";
 
-const formatIconName = (icon: string) => `icon-[${icon.replace(":", "--")}]`;
-
 export const getPostEntries = async () => {
   const postEntries = await getCollection("posts");
   return postEntries
@@ -13,7 +11,7 @@ export const getPostEntries = async () => {
           ...entry,
           data: {
             ...entry.data,
-            icon: formatIconName(entry.data.icon ?? "fluent-emoji-flat:memo"),
+            icon: entry.data.icon ?? "fluent-emoji-flat:memo",
           },
         },
       },
