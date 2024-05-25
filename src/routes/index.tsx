fc88bb88738a8fd0b02956ad6e6f610ea8740168
rai <mj82xx@gmail.com>
2024-05-25 14:21:43 +0900

    refactor: 要らないファイルたちを削除

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { Title } from "@solidjs/meta"
 
-import Counter from "~/components/Counter"
 import { Button } from "~/components/ui/button"
 
 export default function Home() {
@@ -8,15 +7,7 @@ export default function Home() {
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
-      <Counter />
       <Button>Click me</Button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   )
 }
