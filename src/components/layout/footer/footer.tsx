4bbc079048bbb00096c7e52ae15f8329b65ed31a
rai <mj82xx@gmail.com>
2024-05-26 01:03:04 +0900

    feat: footerを実装

---
+++ src/components/layout/footer/footer.tsx
@@ -0,0 +1,17 @@
+import { type Component, type ComponentProps, splitProps } from "solid-js"
+
+import { cn } from "~/libs/utils"
+
+export type FooterProps = Omit<ComponentProps<"footer">, "children">
+
+export const Footer: Component<FooterProps> = (props) => {
+  const [local, rest] = splitProps(props, ["class"])
+  return (
+    <footer
+      class={cn(local.class, "flex w-full flex-col items-center border-t py-4")}
+      {...rest}
+    >
+      <p class="text-sm">Copyright &copy; 2024 r4ai - All rights reserved</p>
+    </footer>
+  )
+}
