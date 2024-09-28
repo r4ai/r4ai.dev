d6bed0865858bf1678f70b228beb5c03e4db4587
rai <mj82xx@gmail.com>
2024-09-28 11:15:44 +0900

    chore: add eslint-plugin-simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import eslint from "@eslint/js"
 import gitignore from "eslint-config-flat-gitignore"
 import eslintConfigPrettier from "eslint-config-prettier"
 import eslintPluginAstro from "eslint-plugin-astro"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 import globals from "globals"
 import tsEslint from "typescript-eslint"
 
@@ -21,6 +22,15 @@ export default tsEslint.config(
   },
   ...eslintPluginAstro.configs["flat/recommended"],
   ...eslintPluginAstro.configs["flat/jsx-a11y-strict"],
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
   eslintConfigPrettier,
   {
     languageOptions: {
