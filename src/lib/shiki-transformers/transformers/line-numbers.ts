8973b6a01d23e006e889223fc18e84cb3f21c907
rai <96561881+r4ai@users.noreply.github.com>
2024-12-20 16:27:38 +0900

    fix: line numbers with diff

--- src/lib/shiki-transformers/transformers/line-numbers.ts
+++ src/lib/shiki-transformers/transformers/line-numbers.ts
@@ -1,19 +1,42 @@
+import type * as hast from "hast"
 import type { ShikiTransformer } from "shiki"
 
 import { parseMeta } from "../utils"
 
-export const transformerLineNumbers = (): ShikiTransformer => ({
-  code() {
-    const meta = parseMeta(this.options.meta?.__raw)
-    if (!meta.showLineNumbers) return
+export const transformerLineNumbers = (): ShikiTransformer => {
+  let startLine: undefined | number = undefined
+  let currentLine: undefined | number = undefined
 
-    this.addClassToHast(this.pre, "has-line-numbers")
-  },
-  line(hast, line) {
-    const meta = parseMeta(this.options.meta?.__raw)
-    if (!meta.showLineNumbers) return
+  return {
+    code(hast) {
+      const meta = parseMeta(this.options.meta?.__raw)
+      if (!meta.showLineNumbers) return
 
-    const startLine = Number(meta.startLine ?? "1")
-    hast.properties["data-line"] = Math.max(startLine - 1, 0) + line
-  },
-})
+      startLine = Number(meta.startLine ?? 1)
+      currentLine = startLine
+
+      this.addClassToHast(this.pre, "has-line-numbers")
+
+      for (const line of hast.children) {
+        if (
+          line.type !== "element" ||
+          (hasClass(line, "diff") && hasClass(line, "remove"))
+        )
+          continue
+
+        line.properties["data-line"] = Math.max(startLine - 1, 0) + currentLine
+        currentLine += 1
+      }
+
+      startLine = undefined
+      currentLine = undefined
+    },
+  }
+}
+
+const hasClass = (hast: hast.Element, className: string) => {
+  const classNames = hast.properties?.class
+  if (!classNames) return false
+  if (Array.isArray(classNames)) return classNames.includes(className)
+  return classNames.toString() === className
+}
