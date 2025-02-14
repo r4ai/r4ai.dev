e8213b763e1f4afb99551134aa1703d6b1df28fa
rai <96561881+r4ai@users.noreply.github.com>
2025-02-14 11:46:35 +0900

    fix: config type name

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-/** @type {import("lint-staged").Config} */
+/** @type {import("lint-staged").Configuration} */
 export default {
   "*.{js,cjs,ts,jsx,tsx,astro}": ["eslint --fix", "prettier --write"],
   "*.{md,html,json,yaml,yml}": ["prettier --write"],
