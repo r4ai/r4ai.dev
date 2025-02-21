006a6bbc7f764c92615deba1ef1225483ab25d12
rai <96561881+r4ai@users.noreply.github.com>
2025-02-22 01:28:25 +0900

    feat: add projects, posts, contact OG images

---
+++ src/pages/contact/index.png.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from "astro"
+import sharp from "sharp"
+
+import { OpenGraphImage, render } from "@/components/open-graph-images"
+
+export const GET: APIRoute = async () => {
+  const image = await render(sharp, OpenGraphImage, {
+    title: "Contact",
+  })
+  return new Response(image, {
+    headers: { "Content-Type": "image/png" },
+  })
+}
