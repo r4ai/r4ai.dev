6f8610b60a4ce68f9726729c5aa6d7a875761df0
rai <96561881+r4ai@users.noreply.github.com>
2025-02-22 16:09:11 +0900

    feat: projects

---
+++ src/pages/projects/_components/link-button/index.ts
@@ -0,0 +1,1 @@
+export { LinkButton, type LinkButtonProps } from "./link-button"
---
+++ src/pages/projects/_components/link-button/link-button.tsx
@@ -0,0 +1,22 @@
+import type { JSXElement } from "solid-js"
+
+import IconExternalLink from "~icons/lucide/external-link"
+
+export type LinkButtonProps = {
+  href: string
+  children?: JSXElement
+}
+
+export const LinkButton = (props: LinkButtonProps) => {
+  return (
+    <a
+      href={props.href}
+      class="flex flex-row items-center gap-1.5 rounded-full border bg-secondary/80 px-4 py-2 text-sm transition hover:bg-secondary"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.children}
+      <IconExternalLink class="size-4" />
+    </a>
+  )
+}
